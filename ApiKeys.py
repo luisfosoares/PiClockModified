8c75d9c9ad0e747343056a5330d2647ccaa130e7
Luis Soares <filetes_6@hotmail.com>
2021-01-31 18:08:31 +0000

    changed api key to allow tests without counting

---
+++ ApiKeys.py
@@ -0,0 +1,8 @@
+
+# DarkSky API key
+dsapi = 'f026b35093bc2ae6503cacc1e67335091'
+# Map API keys -- only needs 1 of the following
+# Google Maps API key (if usemapbox is not set in Config)
+googleapi = 'YOUR GOOGLE MAPS API KEY'
+# Mapbox API key (access_token) [if usemapbox is set in Config]
+mbapi = 'pk.eyJ1IjoibHVpc2Zvc29hcmVzIiwiYSI6ImNrMzNhbDVlcjBhZ3czYnAzcmM2c2ljbjMifQ.8jvftYpO5fHiB9PsK_u8Fw'
